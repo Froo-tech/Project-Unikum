929d56ce58c7fbabaa8dba03486555c7e0df27cf
Frizzi <144421526+Froo-tech@users.noreply.github.com>
2025-01-08 19:41:18 +0700

    Update pass_test.py

--- password_manager/pass_test.py
+++ password_manager/pass_test.py
@@ -1,4 +1,13 @@
 from pass_manager import *
-
-print(reg())
-print(log())
+reg_or_log = 0
+if reg_or_log == 0:
+    reg_or_log = int(input("Введите 1 для входа и 2 для регистрации. "))
+    if reg_or_log == 1:
+        starus = log()
+        data = exe(starus[1])
+        print(f"Ваш username: {data[1]}\nВаша почта: {data[2]}\nВаш возраст: {data[3]}")
+    elif reg_or_log == 2:
+        re = reg()
+        log(re[1],re[2])
+        data = exe(re[1])
+        print(f"Ваш username: {data[1]}\nВаша почта: {data[2]}\nВаш возраст: {data[3]}")

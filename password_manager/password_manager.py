f0ada3f999816264a214ad79644103ee83704519
Frizzi <144421526+Froo-tech@users.noreply.github.com>
2025-01-08 19:40:57 +0700

    Update password_manager.py

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -3,9 +3,8 @@ import hashlib
 
 
 def reg():
-    c = sqlite3.connect("ps1.bd")
+    c = sqlite3.connect("ps1.db")
     cr = c.cursor()
-
 
     cr.execute('''CREATE TABLE IF NOT EXISTS Users (
                     id INTEGER PRIMARY KEY,
@@ -15,45 +14,39 @@ def reg():
                     pas TEXT
                 )''')
 
-
     us = input("username: ")
     email = input("email: ")
 
-    emaill = list(email)
-    while "@" not in emaill:
-        emaill = input("Введите корректный e-mail: ")
-        email = emaill
+    while "@" not in email:
+        email = input("Введите корректный e-mail: ")
 
     age = int(input("age: "))
     pas = input("pas: ")
+
     cr.execute('SELECT * FROM Users WHERE username = ? OR email = ?', (us, email))
     existing_user = cr.fetchone()
 
     if existing_user:
-        if email in existing_user:
+        if existing_user[2] == email:
             print("Пользователь с таким email уже существует!")
             Status_reg = "replay_email"
         else:
             print("Пользователь с таким username уже существует!")
             Status_reg = "replay_username"
     else:
-
-
-
         pash = hashlib.sha256(pas.encode()).hexdigest()
-
         c.execute('INSERT INTO Users (username, email, age, pas) VALUES (?, ?, ?, ?)', (us, email, age, pash))
-
-
         c.commit()
+        Status_reg = "success"
 
     c.close()
-    return Status_reg
+    return Status_reg, us ,pas
 
-def log(bus = None, bpas = None):
-    c = sqlite3.connect("ps1.bd")
+
+def log(bus=None, bpas=None):
+    c = sqlite3.connect("ps1.db")
     cr = c.cursor()
-    if bus != None :
+    if bus is not None:
         username_to_check = bus
         password_to_check = bpas
     else:
@@ -65,7 +58,6 @@ def log(bus = None, bpas = None):
 
     if result:
         stored = result[0]
-
         input_password = hashlib.sha256(password_to_check.encode()).hexdigest()
 
         if input_password == stored:
@@ -79,4 +71,15 @@ def log(bus = None, bpas = None):
         Status_log = False
 
     c.close()
-    return Status_log
+    return Status_log, username_to_check
+
+
+def exe(us):
+    c = sqlite3.connect("ps1.db")
+    cr = c.cursor()
+    cr.execute('SELECT * FROM Users WHERE username=?', (us,))
+    user_data = cr.fetchone()
+    c.close()
+    if user_data != None:
+        return user_data
+

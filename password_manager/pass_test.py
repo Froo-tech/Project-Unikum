07b77f6fd0482b55db0c3f7ab23164bb676e449f
Frizzi <144421526+Froo-tech@users.noreply.github.com>
2025-01-08 13:46:54 +0700

    Update pass_test.py

--- password_manager/pass_test.py
+++ password_manager/pass_test.py
@@ -1,30 +1,4 @@
-import sqlite3
-import hashlib
+from pass_manager import *
 
-
-c = sqlite3.connect("ps1.bd")
-cr = c.cursor()
-
-
-username_to_check = input("Введите имя пользователя: ")
-password_to_check = input("Введите пароль: ")
-
-
-cr.execute('SELECT pas FROM Users WHERE username = ?', (username_to_check,))
-result = cr.fetchone()
-
-
-if result:
-    stored = result[0]
-
-    input_password = hashlib.sha256(password_to_check.encode()).hexdigest()
-
-    if input_password == stored:
-        print("Доступ получен!")
-    else:
-        print("Запрет входа!")
-else:
-    print("Пользователь не найден.")
-
-
-c.close()
+print(reg())
+print(log())

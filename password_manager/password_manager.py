280461c15538dff17f5ea87412081d53e6ccdb77
Frizzi <144421526+Froo-tech@users.noreply.github.com>
2025-01-08 13:46:38 +0700

    Update password_manager.py

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,45 +1,79 @@
 import sqlite3
 import hashlib
 
-c = sqlite3.connect("ps1.bd")
-cr = c.cursor()
+
+def reg():
+    c = sqlite3.connect("ps1.bd")
+    cr = c.cursor()
 
 
-cr.execute('''CREATE TABLE IF NOT EXISTS Users (
-                id INTEGER PRIMARY KEY,
-                username TEXT,
-                email TEXT,
-                age INTEGER,
-                pas TEXT
-            )''')
+    cr.execute('''CREATE TABLE IF NOT EXISTS Users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT,
+                    email TEXT,
+                    age INTEGER,
+                    pas TEXT
+                )''')
 
 
-us = input("username: ")
-email = input("email: ")
+    us = input("username: ")
+    email = input("email: ")
 
-emaill = list(email)
-while "@" not in emaill:
-    emaill = input("Введите корректный e-mail: ")
-    email = emaill
+    emaill = list(email)
+    while "@" not in emaill:
+        emaill = input("Введите корректный e-mail: ")
+        email = emaill
 
-age = int(input("age: "))
-pas = input("pas: ")
-cr.execute('SELECT * FROM Users WHERE username = ? OR email = ?', (us, email))
-existing_user = cr.fetchone()
+    age = int(input("age: "))
+    pas = input("pas: ")
+    cr.execute('SELECT * FROM Users WHERE username = ? OR email = ?', (us, email))
+    existing_user = cr.fetchone()
 
-if existing_user:
-    if email in existing_user:
-        print("Пользователь с таким email уже существует!")
+    if existing_user:
+        if email in existing_user:
+            print("Пользователь с таким email уже существует!")
+            Status_reg = "replay_email"
+        else:
+            print("Пользователь с таким username уже существует!")
+            Status_reg = "replay_username"
     else:
-        print("Пользователь с таким username уже существует!")
-else:
 
 
 
-    pash = hashlib.sha256(pas.encode()).hexdigest()
+        pash = hashlib.sha256(pas.encode()).hexdigest()
 
-    c.execute('INSERT INTO Users (username, email, age, pas) VALUES (?, ?, ?, ?)', (us, email, age, pash))
+        c.execute('INSERT INTO Users (username, email, age, pas) VALUES (?, ?, ?, ?)', (us, email, age, pash))
 
 
-    c.commit()
-c.close()
+        c.commit()
+
+    c.close()
+    return Status_reg
+
+def log():
+    c = sqlite3.connect("ps1.bd")
+    cr = c.cursor()
+
+    username_to_check = input("Введите имя пользователя: ")
+    password_to_check = input("Введите пароль: ")
+
+    cr.execute('SELECT pas FROM Users WHERE username = ?', (username_to_check,))
+    result = cr.fetchone()
+
+    if result:
+        stored = result[0]
+
+        input_password = hashlib.sha256(password_to_check.encode()).hexdigest()
+
+        if input_password == stored:
+            print("Доступ получен!")
+            Status_log = True
+        else:
+            print("Запрет входа!")
+            Status_log = False
+    else:
+        print("Пользователь не найден.")
+        Status_log = False
+
+    c.close()
+    return Status_log
